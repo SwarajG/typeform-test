e15aed7938c82d677d1b12baabbb413ab798d956
SwarajG <gandhiswaraj9067008148@gmail.com>
2019-05-14 11:12:08 +0530

    janky animation removed from the form

--- src/component-helper/index.js
+++ src/component-helper/index.js
@@ -3,26 +3,24 @@ import ReactDOM from 'react-dom';
 export const awayFromTop = ((window.innerHeight * 40) / 100) - 56;
 
 export function moveUp(context, delay = 0) {
-  const { activeIndex, updateActiveIndex, questionRefs, parentRef } = context;
+  const { activeIndex, questionRefs, parentRef } = context;
   const parent = ReactDOM.findDOMNode(parentRef);
   if (activeIndex > 0) {
     const newUpdatedIndex = activeIndex - 1;
     const element = ReactDOM.findDOMNode(questionRefs[newUpdatedIndex]);
     setTimeout(() => {
-      updateActiveIndex(newUpdatedIndex);
       parent.scrollTo(0, element.offsetTop - awayFromTop);
     }, delay);
   }
 }
 
 export function moveDown(context, delay = 0) {
-  const { activeIndex, updateActiveIndex, questions, parentRef, questionRefs } = context;
+  const { activeIndex, questions, parentRef, questionRefs } = context;
   const parent = ReactDOM.findDOMNode(parentRef);
   if (activeIndex + 1 < questions.length) {
     const newUpdatedIndex = activeIndex + 1;
     const element = ReactDOM.findDOMNode(questionRefs[newUpdatedIndex]);
     setTimeout(() => {
-      updateActiveIndex(newUpdatedIndex);
       parent.scrollTo(0, element.offsetTop - awayFromTop);
     }, delay);
   }
